07494d9e668ce92104e4a9732f5ad239c74ecc78
Gabitife <120723899+Gabitife@users.noreply.github.com>
2024-08-27 15:25:32 -0300

    v8.9.1

--- F/o/r/Clara/script.js
+++ F/o/r/Clara/script.js
@@ -1,9 +1,12 @@
+
 document.querySelector('.enter').addEventListener('click', function() {
     document.getElementById('scrollBtn').style.display = 'block'; // Mostra o botão ao clicar
     document.querySelector('.navbar').style.display = 'block'; // Mostra a navbar ao clicar
 });
 
-
-// Adiciona eventos de clique e toque
-enterButton.addEventListener('click', showElements);
-enterButton.addEventListener('touchstart', showElements);
+// Adiciona suporte para toque em dispositivos móveis
+document.querySelector('.enter').addEventListener('touchstart', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão
+    document.getElementById('scrollBtn').style.display = 'block'; // Mostra o botão ao tocar
+    document.querySelector('.navbar').style.display = 'block'; // Mostra a navbar ao tocar
+});
