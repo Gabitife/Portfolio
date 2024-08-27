13e254b79ef209662352c5211a16a071179af049
Gabitife <120723899+Gabitife@users.noreply.github.com>
2024-08-27 15:06:43 -0300

    v8.9

--- F/o/r/Clara/script.js
+++ F/o/r/Clara/script.js
@@ -2,3 +2,8 @@ document.querySelector('.enter').addEventListener('click', function() {
     document.getElementById('scrollBtn').style.display = 'block'; // Mostra o botão ao clicar
     document.querySelector('.navbar').style.display = 'block'; // Mostra a navbar ao clicar
 });
+
+
+// Adiciona eventos de clique e toque
+enterButton.addEventListener('click', showElements);
+enterButton.addEventListener('touchstart', showElements);
