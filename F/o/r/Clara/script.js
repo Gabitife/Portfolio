cd974e750527f011fdf0cdac285edf3fb35ec958
Gabitife <120723899+Gabitife@users.noreply.github.com>
2024-08-27 14:58:01 -0300

    v8.8

---
+++ F/o/r/Clara/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.enter').addEventListener('click', function() {
+    document.getElementById('scrollBtn').style.display = 'block'; // Mostra o botão ao clicar
+    document.querySelector('.navbar').style.display = 'block'; // Mostra a navbar ao clicar
+});
